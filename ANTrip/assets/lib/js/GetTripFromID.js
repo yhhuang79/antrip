e3f3126b9523b97ed815de32078a25bf0566248b
Ivan Chiou <wyattkidd@gmail.com>
2012-08-17 05:05:56 +0000

    fixed lat or lng = -999 point need to be discarded.

--- ANTrip/assets/lib/js/GetTripFromID.js
+++ ANTrip/assets/lib/js/GetTripFromID.js
@@ -23,36 +23,38 @@
 					var lat = point.lat.valueOf() / 1000000;
 					var lng = point.lng.valueOf() / 1000000;
 					var latlng = new google.maps.LatLng(lat, lng);
-					g_tripPointArray.push(latlng);
-					if (typeof point.CheckIn != 'undefined'){
-						var placemarker = self.addMarker({ 
-							'position': latlng, 
-							'bounds': true,
-							'icon': im+"placemarker.png",
-						}).click(function(){
-							var CheckInInfo="";
-							var title="";
-							if(point.CheckIn.picture_uri!=null && typeof point.CheckIn.picture_uri!='undefined'){
-								title = 'http://plash2.iis.sinica.edu.tw/picture/'+sid+"/"+trip_id+"/"+point.CheckIn.picture_uri;
-								CheckInInfo +="<p><img src=" + title + " height='300' /></p>";
-							}
-							if(point.CheckIn.emotion!=null && typeof point.CheckIn.emotion!='undefined'){
-								CheckInInfo +="<p><img width='72px' src='"+im+emotionMapping[point.CheckIn.emotion]+".png'>"+g_Tooltip[emotionMapping[point.CheckIn.emotion]]+"</img></p>";
-							}
-							if(point.CheckIn.message!=null && typeof point.CheckIn.message!='undefined'){
-								CheckInInfo += "<p>"+ point.CheckIn.message +"</p>";
-							}
-							CheckInInfo +="<p>"+lat+", "+lng+"</p>";
-							self.openInfoWindow({'content': CheckInInfo}, this);
-						});
-						g_tripMarkerArray.push(placemarker);
-					} else {
-						self.addMarker({ 
-							'position': latlng, 
-							'bounds': true
-						}).click(function(){
-							self.openInfoWindow({'content': point.timestamp}, this);
-						});
+					if(lat != latlng_undefined_value && lng !=latlng_undefined_value){
+						g_tripPointArray.push(latlng);
+						if (typeof point.CheckIn != 'undefined'){
+							var placemarker = self.addMarker({ 
+								'position': latlng, 
+								'bounds': true,
+								'icon': im+"placemarker.png",
+							}).click(function(){
+								var CheckInInfo="";
+								var title="";
+								if(point.CheckIn.picture_uri!=null && typeof point.CheckIn.picture_uri!='undefined'){
+									title = 'http://plash2.iis.sinica.edu.tw/picture/'+sid+"/"+trip_id+"/"+point.CheckIn.picture_uri;
+									CheckInInfo +="<p><img src=" + title + " height='300' /></p>";
+								}
+								if(point.CheckIn.emotion!=null && typeof point.CheckIn.emotion!='undefined'){
+									CheckInInfo +="<p><img width='72px' src='"+im+emotionMapping[point.CheckIn.emotion]+".png'>"+g_Tooltip[emotionMapping[point.CheckIn.emotion]]+"</img></p>";
+								}
+								if(point.CheckIn.message!=null && typeof point.CheckIn.message!='undefined'){
+									CheckInInfo += "<p>"+ point.CheckIn.message +"</p>";
+								}
+								CheckInInfo +="<p>"+lat+", "+lng+"</p>";
+								self.openInfoWindow({'content': CheckInInfo}, this);
+							});
+							g_tripMarkerArray.push(placemarker);
+						} else{
+							self.addMarker({ 
+								'position': latlng, 
+								'bounds': true
+							}).click(function(){
+								self.openInfoWindow({'content': point.timestamp}, this);
+							});
+						}
 					}
 				});
 				self.addShape('Polyline',{
