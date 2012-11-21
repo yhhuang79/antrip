628ef00a4035c610bcf58ccf6e59843dd14223bc
Christopher Szu <cszu@iis.sinica.edu.tw>
2012-11-21 02:01:07 +0000

    fixed trip length > 6000km when actual distance should be near zero

--- ANTrip/src/tw/plash/antrip/TripStats.java
+++ ANTrip/src/tw/plash/antrip/TripStats.java
@@ -5,19 +5,19 @@ import java.sql.Timestamp;
 import android.location.Location;
 
 public class TripStats {
-	//time when start record button is pushed
+	// time when start record button is pushed
 	private String buttonStartTime;
-	//time of the LAST inaccurate point
+	// time of the LAST inaccurate point
 	private String lastInaccuratePointTime;
-	//time of the GOOD good point
+	// time of the GOOD good point
 	private String lastGoodPointTime;
-	//time when end record button is pushed
+	// time when end record button is pushed
 	private String buttonEndTime;
-	//in meters
+	// in meters
 	private final Double radius = 6371008.7714;
-	//in meters
+	// in meters
 	private Double totalValidLength;
-	//total points
+	// total points
 	private Integer totalPointCount;
 	// exclude -999 points
 	private Integer totalValidPointCount;
@@ -25,7 +25,7 @@ public class TripStats {
 	private Integer totalAccuratePointCount;
 	// regardless of accuracy
 	private Integer totalCheckinPointCount;
-	//holder
+	// holder
 	private Location previousLoc;
 	
 	public TripStats() {
@@ -43,67 +43,70 @@ public class TripStats {
 		previousLoc = null;
 	}
 	
-	synchronized public void addOnePoint(Location loc, boolean checkin){
-		//don't proceed if null was inputted
-		if(loc != null){
-			//input not null, add one count
+	synchronized public void addOnePoint(Location loc, boolean checkin) {
+		// ignore null points
+		if (loc != null) {
+			// input not null, add one count
 			totalPointCount += 1;
-			//if previous location was not null, it is not the first time adding point
-			if(previousLoc != null){
-				if(checkin){
-					//is a check-in point, skip all validity check
-					totalCheckinPointCount += 1;
-					//regardless of accuracy, check-in points are to be kept in DB
-					totalValidLength += greatCircleDistance(previousLoc, loc);
-					//replace the location in holder with current location
-					previousLoc = loc;
-				} else{
-					// not a check-in point, check for validity and accuracy
-					if(LocationFilter.validityFilter(loc)){
-						totalValidPointCount += 1;
-						lastInaccuratePointTime = new Timestamp(loc.getTime()).toString();
-						if(LocationFilter.accuracyFilter(loc)){
+			// ignore -999 points at any stage
+			if (LocationFilter.validityFilter(loc)) {
+				totalValidPointCount += 1;
+				lastInaccuratePointTime = new Timestamp(loc.getTime()).toString();
+				// if previous location was not null, it is not the first time
+				// adding point
+				if (previousLoc != null) {
+					if (checkin) {
+						// is a check-in point, skip all validity check
+						totalCheckinPointCount += 1;
+						// regardless of accuracy, check-in points are to be
+						// kept in DB
+						totalValidLength += greatCircleDistance(previousLoc, loc);
+						// replace the location in holder with current location
+						previousLoc = loc;
+					} else {
+						// not a check-in point, check for validity and accuracy
+						if (LocationFilter.accuracyFilter(loc)) {
 							totalAccuratePointCount += 1;
 							lastGoodPointTime = new Timestamp(loc.getTime()).toString();
-							//input location is valid, calculate the distance
+							// input location is valid, calculate the distance
 							totalValidLength += greatCircleDistance(previousLoc, loc);
-							//replace the location in holder with current location
+							// replace the location in holder with current
+							// location
 							previousLoc = loc;
-						} else{
-							//inaccurate points
+						} else {
+							// inaccurate points
 						}
-					} else{
-						//invalid points
 					}
+				} else {
+					// previous location was null, first time adding point
+					previousLoc = loc;
 				}
-			} else{
-				//previous location was null, first time adding point
-				previousLoc = loc;
 			}
 		}
 	}
 	
-	private Double greatCircleDistance(Location one, Location two){
+	private Double greatCircleDistance(Location one, Location two) {
 		Double dlat = toRad(two.getLatitude() - one.getLatitude());
 		Double dlon = toRad(two.getLongitude() - one.getLongitude());
 		Double latone = toRad(one.getLatitude());
 		Double lattwo = toRad(two.getLatitude());
-		Double a = Math.sin(dlat/2) * Math.sin(dlat/2) + Math.sin(dlon/2) * Math.sin(dlon/2) * Math.cos(latone) * Math.cos(lattwo);
+		Double a = Math.sin(dlat / 2) * Math.sin(dlat / 2) + Math.sin(dlon / 2) * Math.sin(dlon / 2) * Math.cos(latone)
+				* Math.cos(lattwo);
 		Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 		return radius * c;
 	}
 	
-	private Double toRad(Double degree){
-		return degree/180*Math.PI;
+	private Double toRad(Double degree) {
+		return degree / 180 * Math.PI;
 	}
-
-	public String getNonNullEndTime(){
-		if(buttonEndTime != null){
+	
+	public String getNonNullEndTime() {
+		if (buttonEndTime != null) {
 			return buttonEndTime;
-		} else if(lastInaccuratePointTime != null){
+		} else if (lastInaccuratePointTime != null) {
 			return lastInaccuratePointTime;
-		} else{
-			//no further value available, must return a value
+		} else {
+			// no further value available, must return a value
 			return lastGoodPointTime;
 		}
 	}
@@ -111,43 +114,43 @@ public class TripStats {
 	public String getButtonStartTime() {
 		return buttonStartTime;
 	}
-
+	
 	public void setButtonStartTime(String buttonStartTime) {
 		this.buttonStartTime = buttonStartTime;
 	}
-
+	
 	public String getLastInaccuratePointTime() {
 		return lastInaccuratePointTime;
 	}
-
+	
 	public String getLastGoodPointTime() {
 		return lastGoodPointTime;
 	}
-
+	
 	public String getButtonEndTime() {
 		return buttonEndTime;
 	}
-
+	
 	public void setButtonEndTime(String buttonEndTime) {
 		this.buttonEndTime = buttonEndTime;
 	}
-
+	
 	public Double getTotalValidLength() {
 		return totalValidLength;
 	}
-
+	
 	public Integer getTotalPointCount() {
 		return totalPointCount;
 	}
-
+	
 	public Integer getTotalValidPointCount() {
 		return totalValidPointCount;
 	}
-
+	
 	public Integer getTotalAccuratePointCount() {
 		return totalAccuratePointCount;
 	}
-
+	
 	public Location getPreviousLoc() {
 		return previousLoc;
 	}
